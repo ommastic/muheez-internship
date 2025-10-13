c2a3bf17edc08c26b991d3c58587fd10b8968d55
muheez <muheezopebiyi@gmail.com>
2025-10-12 22:25:25 -0400

    created and make changes to hot collections: using axios to get the data, print it, created a slider, and skeleton for loading

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,46 +1,125 @@
-import React from "react";
+import React, { useEffect, useRef, useState, useMemo } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import axios from "axios";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-const HotCollections = () => {
+
+export default function HotCollections() {
+  const [collections, setCollections] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const sliderRef = useRef(null);
+
+
+  useEffect(() => {
+    let cancelled = false;
+    (async () => {
+      try {
+        const { data } = await axios.get(
+          "https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections"
+        );
+        if (!cancelled) {
+          setCollections(Array.isArray(data) ? data : Object.values(data || {}));
+          setLoading(false);
+        }
+      } catch (e) {
+        if (!cancelled) {
+          console.error("hotCollections fetch failed:", e);
+          setCollections([]);
+          setLoading(false);
+        }
+      }
+    })();
+    return () => { cancelled = true; };
+  }, []);
+
+  const settings = useMemo(() => ({
+    arrows: true,
+    dots: false,
+    infinite: true,
+    speed: 450,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    swipeToSlide: true,
+    responsive: [
+      { breakpoint: 1100, settings: { slidesToShow: 3 } },
+      { breakpoint: 992, settings: { slidesToShow: 2 } },
+      { breakpoint: 600, settings: { slidesToShow: 1 } },
+    ],
+  }), []);
+
+
+  const canSlide = collections.length > 4;
+
   return (
     <section id="section-collections" className="no-bottom">
       <div className="container">
-        <div className="row">
-          <div className="col-lg-12">
-            <div className="text-center">
-              <h2>Hot Collections</h2>
-              <div className="small-border bg-color-2"></div>
-            </div>
+        <div className="col-lg-12">
+          <div className="text-center">
+            <h2>Hot Collections</h2>
+            <div className="small-border bg-color-2" />
           </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="nft_coll">
-                <div className="nft_wrap">
-                  <Link to="/item-details">
-                    <img src={nftImage} className="lazy img-fluid" alt="" />
-                  </Link>
+        </div>
+
+        <div className="relative">
+          {loading ? (
+            // Skeletons
+            <div className="skeleton-row">
+              {[...Array(4)].map((_, i) => (
+                <div className="sk-card" key={i}>
+                  <div className="nft_coll">
+                    <div className="nft_wrap">
+                      <div className="sk sk-img" />
+                    </div>
+
+                    <div className="nft_coll_pp">
+                      <div className="sk sk-avatar" />
+                      <div className="sk sk-badge" />
+                    </div>
+
+                    <div className="nft_coll_info">
+                      <div className="sk sk-title" />
+                      <div className="sk sk-sub" />
+                    </div>
+                  </div>
                 </div>
-                <div className="nft_coll_pp">
-                  <Link to="/author">
-                    <img className="lazy pp-coll" src={AuthorImage} alt="" />
-                  </Link>
-                  <i className="fa fa-check"></i>
-                </div>
-                <div className="nft_coll_info">
-                  <Link to="/explore">
-                    <h4>Pinky Ocean</h4>
-                  </Link>
-                  <span>ERC-192</span>
-                </div>
-              </div>
+              ))}
             </div>
-          ))}
+          ) : (
+            // Real slider
+            <Slider {...settings}>
+              {collections.map((item) => (
+                <div className="nft_coll" key={item.id}>
+                  <div className="nft_wrap">
+                    <Link to="/item-details">
+                      <img
+                        src={item.nftImage}
+                        className="lazy img-fluid"
+                        alt={item.title}
+                      />
+                    </Link>
+                  </div>
+
+                  <div className="nft_coll_pp">
+                    <Link to="/author">
+                      <img className="lazy pp-coll" src={item.authorImage} alt="" />
+                    </Link>
+                    <i className="fa fa-check" />
+                  </div>
+
+                  <div className="nft_coll_info">
+                    <Link to="/explore">
+                      <h4>{item.title}</h4>
+                    </Link>
+                    <span>ERC-{item.code}</span>
+                  </div>
+                </div>
+              ))}
+            </Slider>
+          )}
         </div>
       </div>
     </section>
   );
-};
-
-export default HotCollections;
+}
