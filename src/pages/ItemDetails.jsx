97c237980c631a55baaa115845ac6cc7c769dfd1
muheez <muheezopebiyi@gmail.com>
2025-11-02 11:12:25 -0500

    completed the skeletal work

--- src/pages/ItemDetails.jsx
+++ src/pages/ItemDetails.jsx
@@ -37,91 +37,148 @@ const ItemDetails = () => {
     return () => controller.abort();
   }, [nftId]);
 
-
   return (
-  loading ? (
-  <div className="container py-5 text-center">Loading...</div>
-    ) : details ? (
+    // skeleton
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
         <div id="top"></div>
-        <section aria-label="section" className="mt90 sm-mt-0">
-          <div className="container">
-            <div className="row">
 
-              <div className="col-md-6 text-center">
-                <img
-                  src={details.nftImage}
-                  className="img-fluid img-rounded mb-sm-30 nft-image"
-                  alt=""
-                />
-              </div>
-              <div className="col-md-6">
-                <div className="item_info">
-                  <h2>{details.title}</h2>
+        {loading ? (
+
+          <section aria-label="section" className="mt90 sm-mt-0 itemdetails-skeleton">
+            <div className="container">
+              <div className="row main-class">
+                {/* left NFT image */}
+                <div className="col col-md-6 fte-ske" >
+                  <div className="ske ske-media ske-shimmer" />
+                </div>
+
+                {/* Right side */}
+                <div className="col col-md-6 header-ske">
+
+                  <div className="title-bar ske-shimmer ske" />
+
+                  <div className="col-right">
+                    <div className="ske ske-line ske-line-lg ske-shimmer" />
+                  </div>
 
                   <div className="item_info_counts">
                     <div className="item_info_views">
-                      <i className="fa fa-eye"></i>
-                      {details.views}
+                      <div className="ske-chip" />
                     </div>
                     <div className="item_info_like">
-                      <i className="fa fa-heart"></i>
-                      {details.likes}
+                      <div className="ske-chip" />
                     </div>
                   </div>
-                  <p>
-                    {details.description}
-                  </p>
-                  <div className="d-flex flex-row">
-                    <div className="mr40">
-                      <h6>Owner</h6>
-                      <div className="item_author">
-                        <div className="author_list_pp">
-                          <Link to="/author">
-                            <img className="lazy" src={details.ownerImage} alt={details.ownerName}/>
-                            <i className="fa fa-check"></i>
-                          </Link>
-                        </div>
-                        <div className="author_list_info">
-                          <Link to={`/author/${details.ownerId}`}>{details.ownerName}</Link>
-                        </div>
-                      </div>
-                    </div>
-                    <div></div>
+
+                  <div>
+                    <div className="ske message-block ske-shimmer" />
                   </div>
-                  <div className="de_tab tab_simple">
-                    <div className="de_tab_content">
-                      <h6>Creator</h6>
-                      <div className="item_author">
-                        <div className="author_list_pp">
-                          <Link to={`/author/${details.ownerId}`}>
-                            <img className="lazy" src={details.creatorImage} alt={details.creatorName}/>
-                            <i className="fa fa-check"></i>
-                          </Link>
-                        </div>
-                        <div className="author_list_info">
-                          <Link to={`/author/${details.ownerId}`}>{details.creatorName}</Link>
-                        </div>
+
+                  <div className="spacer-20"></div>
+                  <h6>Owner</h6>
+                  <div className="item_author">
+                    <div className="ske ske-avatar ske-shimmer" />
+                      <div className="ske ske-line ske-shimmer" />
+                  </div>
+
+                <div className="spacer-20"></div>
+                <h6>Creator</h6>
+                <div className="item_author">
+                  <div className="ske ske-avatar ske-shimmer" />
+                  <div className="ske ske-line ske-shimmer" />
+                </div>
+
+                <div className="spacer-40"></div>
+                {/* price */}
+                <h6>Price</h6>
+                <div className="nft-item-price">
+                  <div className="ske ske-icon ske-shimmer" />
+                  <div className="ske ske-line ske-shimmer" />
+                </div>
+              </div>
+            </div>
+          </div>
+          </section>
+
+      ) : details ? (
+
+      <section aria-label="section" className="mt90 sm-mt-0">
+        <div className="container">
+          <div className="row">
+
+            <div className="col-md-6 text-center">
+              <img
+                src={details.nftImage}
+                className="img-fluid img-rounded mb-sm-30 nft-image"
+                alt=""
+              />
+            </div>
+            <div className="col-md-6">
+              <div className="item_info">
+                <h2>{details.title}</h2>
+
+                <div className="item_info_counts">
+                  <div className="item_info_views">
+                    <i className="fa fa-eye"></i>
+                    {details.views}
+                  </div>
+                  <div className="item_info_like">
+                    <i className="fa fa-heart"></i>
+                    {details.likes}
+                  </div>
+                </div>
+                <p>
+                  {details.description}
+                </p>
+                <div className="d-flex flex-row">
+                  <div className="mr40">
+                    <h6>Owner</h6>
+                    <div className="item_author">
+                      <div className="author_list_pp">
+                        <Link to={`/author/${details.ownerId}`}>
+                          <img className="lazy" src={details.ownerImage} alt={details.ownerName} />
+                          <i className="fa fa-check"></i>
+                        </Link>
+                      </div>
+                      <div className="author_list_info">
+                        <Link to={`/author/${details.ownerId}`}>{details.ownerName}</Link>
                       </div>
                     </div>
-                    <div className="spacer-40"></div>
-                    <h6>Price</h6>
-                    <div className="nft-item-price">
-                      <img src={EthImage} alt="" />
-                      <span>{details.price}</span>
+                  </div>
+                </div>
+                <div className="de_tab tab_simple">
+                  <div className="de_tab_content">
+                    <h6>Creator</h6>
+                    <div className="item_author">
+                      <div className="author_list_pp">
+                        <Link to={`/author/${details.ownerId}`}>
+                          <img className="lazy" src={details.creatorImage} alt={details.creatorName} />
+                          <i className="fa fa-check"></i>
+                        </Link>
+                      </div>
+                      <div className="author_list_info">
+                        <Link to={`/author/${details.ownerId}`}>{details.creatorName}</Link>
+                      </div>
                     </div>
+                  </div>
+                  <div className="spacer-40"></div>
+                  <h6>Price</h6>
+                  <div className="nft-item-price">
+                    <img src={EthImage} alt="" />
+                    <span>{details.price}</span>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </section>
-      </div>
+        </div>
+      </section>
+        ) : null}
     </div>
-  ): null
-  )   
- };
+    </div >
+  )
+};
 
 
 
